64dc3e018d4d4399e65644eeac4201ae42c71d55
Jörgen <kejsardamberg@hotmail.com>
2017-03-28 08:50:03 +0200

    Broken link checker now only checks status code, not whole response body.

--- TestingCapabilityWeb/src/main/java/se/claremont/autotest/websupport/LinkCheck.java
+++ TestingCapabilityWeb/src/main/java/se/claremont/autotest/websupport/LinkCheck.java
@@ -2,7 +2,6 @@ package se.claremont.autotest.websupport;
 
 import se.claremont.autotest.common.logging.LogLevel;
 import se.claremont.autotest.common.testcase.TestCase;
-import se.claremont.autotest.restsupport.RestResponse;
 import se.claremont.autotest.restsupport.RestSupport;
 
 /**
@@ -33,18 +32,19 @@ public class LinkCheck implements Runnable{
                 log(LogLevel.INFO, "Reference to mail address (MailTo:) not checked for validity. Reference: '" + link + "'.");
                 return;
             }
-            RestResponse restResponse = new RestSupport(testCase).responseFromGetRequest(link);
-            responseCode = restResponse.responseCode;
+            responseCode = new RestSupport(testCase).responseCodeFromGetRequest(link);
         } catch (Exception e) {
             try {
                 log(LogLevel.VERIFICATION_PROBLEM, "Could not verify link '" + link + "' (response took " + (System.currentTimeMillis() - startTime)  + " milliseconds). Error: " + e.getMessage());
             } catch (Exception e1){
-                log(LogLevel.FRAMEWORK_ERROR, "Could not verify one link '" + link + "'. Error: " + e.getMessage());
+                log(LogLevel.FRAMEWORK_ERROR, "Could not verify link '" + link + "'. Error: " + e.getMessage());
             }
         }
-        if(responseCode != null && responseCode.equals("200")){
+        if(responseCode != null && (responseCode.startsWith("2") || responseCode.startsWith("3"))){
             log(LogLevel.VERIFICATION_PASSED, "Link '" + link + "' is ok (response took " + (System.currentTimeMillis() - startTime) + " milliseconds). Response code '" + responseCode + "'");
-        } else {
+        } else if(responseCode == null){
+            log(LogLevel.VERIFICATION_PROBLEM, "No response at all for link '" + link + "'.");
+        } else{
             log(LogLevel.VERIFICATION_FAILED, "Link '" + link + "' was broken (response took " + (System.currentTimeMillis() - startTime) + " milliseconds). Response code '" + responseCode + "'.");
         }
 
