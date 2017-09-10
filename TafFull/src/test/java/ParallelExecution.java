eeeb34c5c42edf04ffa4788fff4adf39a8d9de4a
Jörgen <kejsardamberg@hotmail.com>
2017-09-10 23:11:16 +0200

    Improved unit tests

--- TafFull/src/test/java/ParallelExecution.java
+++ TafFull/src/test/java/ParallelExecution.java
@@ -1,10 +1,19 @@
 package se.claremont.autotest.common.testset;
 
+import org.junit.Assert;
 import org.junit.Test;
 import se.claremont.autotest.common.logging.LogLevel;
 import se.claremont.autotest.common.testcase.TestCase;
 
 public class ParallelExecution extends TestSet{
+
+    private void failingNonTestPrivateMethodBeingCallable(){
+        System.out.println("Private non-test method executed");
+    }
+
+    public void failingNonTestPublicMethodBeingCallable(){
+        System.out.println("Public non-test method executed");
+    }
 
     @Test
     public void parallelExecutionTestCaseStockholm(){
