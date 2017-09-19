894aed64dda1fa699dcf7094d35a6f5e031cb516
Jörgen <kejsardamberg@hotmail.com>
2017-09-19 12:42:11 +0200

    Added unit test for scrollbar presence.

--- TafFull/src/test/java/TafSystemTests.java
+++ TafFull/src/test/java/TafSystemTests.java
@@ -1,6 +1,7 @@
 import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import se.claremont.autotest.common.logging.LogFolder;
@@ -53,24 +54,28 @@ public class TafSystemTests extends TestSet{
     }
 
     @Test
-    @Ignore
+    @Ignore("Run only where you can clean your test log folder")
     public void scrollbarForLongLogPostMessages(){
         TestCase testCase = new TestCase();
         testCase.testCaseResult.testCaseLog.log(LogLevel.INFO, "Short message", "<br>Short message<br>", "Noname testcase", "Step1", "TestClass");
         testCase.testCaseResult.testCaseLog.log(LogLevel.EXECUTED, "Long message 1234567890 1234567890 1234567890 1234567890 1234567890 1234567890 1234567890 1234567890 1234567890 1234567890 1234567890 1234567890 1234567890 1234567890 1234567890 1234567890 12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890", "Longmessage123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890".replace(" ", ""), "Noname testcase", "Step2", "TestClass");
         testCase.report();
+        //WebDriver driver = new HtmlUnitDriver();
+        //WebInteractionMethods web = new WebInteractionMethods(currentTestCase(), driver);
         WebInteractionMethods web = new WebInteractionMethods(currentTestCase());
+        Assume.assumeTrue("Not able to execute Javascript with this driver", web.driver instanceof JavascriptExecutor);
         web.navigate(testCase.pathToHtmlLogFile);
         web.click(new DomElement("Step2", DomElement.IdentificationType.BY_VISIBLE_TEXT));
         web.wait(1000);
         String xpath = "//td[contains(text(),'Longmessage')]";
         WebElement logMessage = web.driver.findElement(By.xpath(xpath));
+        Assert.assertNotNull(logMessage);
         JavascriptExecutor js = (JavascriptExecutor)web.driver;
-        currentTestCase().log(LogLevel.INFO, logMessage.toString());
-        currentTestCase().log(LogLevel.INFO, new DomElement(logMessage).toString());
-//        currentTestCase().log(LogLevel.INFO, (String)js.executeScript("return argument[0]", logMessage).toString());
-        Assert.assertTrue((boolean)js.executeScript("return arguments[0].scrollWidth>arguments[0].clientWidth;", logMessage));
 
+        int elementWidth = ((Long) js.executeScript("return arguments[0].scrollWidth", logMessage)).intValue();
+        int browserWidth = web.driver.manage().window().getSize().width;
+        Assert.assertTrue(elementWidth > browserWidth);
+        web.makeSureDriverIsClosed();
     }
 
 
