eac72375e5ada07f8d7a93423bff8e2c57e6720f
Jörgen <kejsardamberg@hotmail.com>
2017-12-18 14:13:23 +0100

    Added missing files

---
+++ TafFull/src/main/java/se/claremont/autotest/TAF.java
@@ -0,0 +1,10 @@
+package se.claremont.autotest;
+
+import se.claremont.autotest.common.testrun.CliTestRunner;
+
+public class TAF {
+
+    public static void main(String[] args){
+        CliTestRunner.main(args);
+    }
+}
